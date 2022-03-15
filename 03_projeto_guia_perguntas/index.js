55acfb39c76560de2aa90ff350f6e6d977312f36
Caique Souza <caiqueubasp@gmail.com>
2022-03-15 19:26:41 -0300

    using body parser and POST form informations

--- 03_projeto_guia_perguntas/index.js
+++ 03_projeto_guia_perguntas/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 // const home = require("./views/index.ejs");
 
 // Estou dizendo para o Express usar o EJS como View engine
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// Rotas
 const port = 8050;
 
 app.get("/", (req, res) => {
@@ -14,6 +18,12 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  res.send("Formulário recebido" + titulo + " " + descricao);
 });
 
 app.listen(port, () => {
